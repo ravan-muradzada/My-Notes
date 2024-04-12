97ce39d40c7c3f96555cc1e856f6d1dc374eb715
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-04-12 07:54:28 +0400

    BFS and DFS (undirected graph).cpp

--- BFS and DFS (undirected graph).cpp
+++ BFS and DFS (undirected graph).cpp
@@ -3,12 +3,12 @@
 #include <queue>
 using namespace std;
 // BFS and DFS in C++, for undirected graph
-void bfs(int n, vector<int> arr[], vector<bool>& visited){
+void bfs(int n, vector<int> adjacencyList[], vector<bool>& visited){
     queue<int> q;
     q.push(1); visited[1] = true;
     while (!q.empty()){
         cout << q.front() << " ";
-        for (int i: arr[q.front()]){
+        for (int i: adjacencyList[q.front()]){
             if (visited[i] != true){
                 q.push(i);
                 visited[i] = true;
@@ -29,17 +29,17 @@ void dfs(int node, vector<int> arr[], vector<bool>& visited){
 int main(){
     int n, m, from, to; // nodes, roads
     cin >> n >> m;
-    vector<int> arr[n + 1];
+    vector<int> adjacencyList[n + 1];
     
     for (int i = 0; i < m; ++i){
         cin >> from >> to;
-        arr[from].push_back(to);
-        arr[to].push_back(from);
+        adjacencyList[from].push_back(to);
+        adjacencyList[to].push_back(from);
     }
     vector<bool>visited(n + 1, false);
-    bfs(n, arr, visited);
+    bfs(n, adjacencyList, visited);
     visited.assign(n + 1, false);
-    dfs(1, arr, visited);
+    dfs(1, adjacencyList, visited);
     
     return 0;
 }
