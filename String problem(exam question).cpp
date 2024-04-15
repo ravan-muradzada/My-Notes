02ee7ed11c296ccd586b17ae0fbff7cbbde41e48
mrRavanCpp <161654833+mrRavanCpp@users.noreply.github.com>
2024-04-15 16:57:22 +0400

    String problem(exam question).cpp

--- String problem(exam question).cpp
+++ String problem(exam question).cpp
@@ -8,15 +8,18 @@ int main(){
     getline(cin, t);
     sort(s.begin(), s.end());
     sort(t.begin(), t.end());
+    
     int ptr1 = 0, ptr2 = 0;
-    while (ptr1 < s.size() || ptr2 < t.size()){
-        if (ptr1 >= s.size() || s[ptr1] != t[ptr2]){
+    
+    while (true){
+        if (s[ptr1] != t[ptr2]){
             cout << t[ptr2];
             break;
         }
         ptr1++; 
         ptr2++;
     }
+    
     
     return 0;
 }
